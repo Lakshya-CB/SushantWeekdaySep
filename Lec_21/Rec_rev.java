9540ba9926935eadfac02675786982a589db2ffe
Lakshya Singh <78645656+Lakshya-CB@users.noreply.github.com>
2023-12-27 12:37:54 +0530

    Add files via upload

--- Lec_21/Rec_rev.java
+++ Lec_21/Rec_rev.java
@@ -3,25 +3,29 @@ package Lec_21;
 import java.util.Scanner;
 
 public class Rec_rev {
+	static int cnt = 0;
 
 	public static void main(String[] args) {
 //		System.out.println(Fact(4));
 		Scanner scn = new Scanner(System.in);
-		SS(scn.next(),"");
+//		SS(scn.next(), "");
+		SSAscii(scn.next(), "");
+		System.out.println(cnt);
 	}
 
 	public static void SS(String frens, String team) {
-		if(frens.isEmpty()) {
+		if (frens.isEmpty()) {
 			System.out.println(team);
+			cnt++;
 			return;
 		}
 //		BP =>"abcd"
 //		SP => "bcd"
 		String sp = frens.substring(1);
 		char ch = frens.charAt(0);
-		SS(sp, team+ch);
+		SS(sp, team + ch);
 		SS(sp, team);
-		
+
 	}
 
 	public static int Fact(int n) {
@@ -32,5 +36,19 @@ public class Rec_rev {
 //		SP : n-1!
 		int sp = Fact(n - 1);
 		return sp * n;
+	}
+
+	public static void SSAscii(String frens, String team) {
+		if (frens.isEmpty()) {
+			System.out.println(team);
+			cnt++;
+			return;
+		}
+		String sp = frens.substring(1);
+		char ch = frens.charAt(0);
+		SSAscii(sp, team);
+		SSAscii(sp, team + ch);
+		SSAscii(sp, team + (int)ch);
+
 	}
 }
